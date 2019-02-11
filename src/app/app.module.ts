a3e40b7ebee92de282ef79cefd4dfce50a988193
Carlos Herrera Conejero <carherco@gmail.com>
2019-02-11 17:36:07 +0100

    ejemplo de configuración

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,14 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot({
+      tabButtonLayout: 'icon-bottom',
+      mode: 'ios'
+    }), 
+    AppRoutingModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
